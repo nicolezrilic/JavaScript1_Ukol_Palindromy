0a130e19069f386b080b78f1345bef4bc623d8d2
Nicole Zrilic <nikol.zrilicova@gmail.com>
2025-12-07 20:47:41 +0100

    Finální verze

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-console.log('Funguju!')
+console.log('Funguju!');
 
+const items = document.querySelectorAll('li');
+        
 const textPozpatku = (text) => {
-	return text.split('').reverse().join('')
-}
+    return text.split('').reverse().join('');
+};
 
-// textPozpatku('Ahoj světe') // 'etěvs johA'
-// textPozpatku('Kajak') // 'kajaK'
+items.forEach ((li) => {
+    const raw = li.textContent.trim().toLowerCase();   //text v odrážce - odstraním mezery na začátku a konci, převedu na malá písmena
+    const text = raw.replace(/\s+/g, ""); // odstranění mezer uvnitř slovních spojení
+
+    if (text === textPozpatku(text)) {  //pokud jde o palindrom, přidej mu třídu 'palindrom'
+        li.classList.add('palindrom');
+  }
+});
+
+
+    // textPozpatku('Ahoj světe') // 'etěvs johA'
+    // textPozpatku('Kajak') // 'kajaK'
